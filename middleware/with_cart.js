bdafcb0dad67c2f451dd58ff9ff237ccf40bde37
aemcee <acapili07@gmail.com>
2019-07-17 04:03:36 +0000

    withcart and getcart in class completed

--- middleware/with_cart.js
+++ middleware/with_cart.js
@@ -11,17 +11,63 @@ module.exports = async (req, res, next) => {
         if(cartToken){
             const cartData = jwt.decode(cartToken, cartSecret);
 
+            console.log('cartData from withCart: ', cartData); 
             // cartData.cartId is the data received on postman, which comes from cartToken
             // did not use execute because even though client side sent data the data is decrypted
             const [cart = null] = await db.query(
-                `SELECT * FROM carts AS c JOIN cartItems AS ci ON ci.cartId = c.id WHERE c.id=${cartData.cartId} AND c.deletedAt IS NULL AND ci.deletedAt is NULL`
+                `SELECT c.id AS cartId, c.lastInteraction, c.pid, c.createdAt, c.updatedAt, c.userId, c.statusId AS cartStatusId, ci.quantity, p.cost FROM carts AS c 
+                JOIN cartItems AS ci ON ci.cartId = c.id 
+                JOIN products AS p ON ci.productId = p.id 
+                WHERE c.id=${cartData.cartId} AND c.deletedAt IS NULL AND ci.deletedAt is NULL`
             );
 
             if(!cart){
                 throw new StatusError(422, 'Invalid cart token');
             }
 
-            req.cart = cart;
+            // doesnt matter, pulled off first item in cart
+            const {cost, quantity, ...cartItem} = cart[0];
+            // console.log('Cost from destructuring: ', cost); 
+            // console.log('Cart: ', cart); 
+            // console.log('Cart Item: ', cartItem); 
+
+            // first method
+            // const formattedCart = {
+            //     ...cartItem,
+            //     items: cart.map(({cost, quantity}) => ({cost, quantity}))
+            // };
+            // whatever follows the fat arrow is returned
+
+            // second method
+            const formattedCart = {
+                ...cartItem,
+                items: cart.map((item) => {
+                    // console.log('Item in Map: ', item); 
+                    return {
+                        cost: item.cost,
+                        quantity: item.quantity
+                    }
+                })
+            };
+
+            // third method
+            // cart.forEach(item => {
+                // formattedCart.id = item.id;
+                // formattedCart.lastInteraction = item.lastInteraction;
+                // formattedCart.pid = item.pid;
+                // formattedCart.createdAt = item.createdAt;
+                // formattedCart.updatedAt = item.updatedAt;
+                // formattedCart.userId = item.userId;
+                // formattedCart.cartStatusId = item.cartStatusId;
+
+            //     formattedCart.items.push({
+            //         cost: item.cost,
+            //         quantity: item.quantity
+            //     });
+            // });
+
+            req.cart = formattedCart;
+            // req.cart = cart;
         };
 
         // sample data to test pathways
--- routes/api/controllers/cart/get_cart.js
+++ routes/api/controllers/cart/get_cart.js
@@ -1,12 +1,61 @@
-module.exports = (req, res, next) => {
+const db = require(__root + '/db'); 
+const {imageUrl} = require(__root + '/helpers'); 
+
+module.exports = async (req, res, next) => {
     try{
-        // delted due to middle ware add for repetivite decrease
+        // deleted due to middle ware add for repetivite decrease
         // const {headers} = req;
         // const {'x-cart-token': cartToken} = headers;
 
+        let {cart} = req;
+        let cartDataToSend = null; 
+
+        // filter if no cart
+        if(cart){
+            const [cartItems] = await db.query(`SELECT ci.createdAt AS added, p.cost AS 'each', ci.pid AS itemId, p.name, p.pid AS productId, ci.quantity, i.altText, i.type, i.file FROM cartItems AS ci
+            JOIN products AS p on ci.productId=p.id
+            JOIN images AS i on p.thumbnailId=i.id 
+            WHERE ci.cartId = ${cart.cartId} AND ci.deletedAt is NULL`);
+
+            // running cart total
+            const total = {
+                cost: 0,
+                items: 0
+            }; 
+
+            const items = cartItems.map(item => {
+                const itemTotal = item.quantity * item.each;
+
+                total.cost += itemTotal;
+                total.items += item.quantity;
+
+                return {
+                    added: item.added,
+                    each: item.each,
+                    itemId: item.itemId,
+                    name: item.name,
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    thumbnail: {
+                        altText: item.altText,
+                        url: imageUrl(req, item.type, item.file)
+                    },
+                    total: itemTotal
+                }
+            });
+
+            cartDataToSend = {
+                cartId: cart.pid,
+                items: items,
+                total: total
+            }
+
+            console.log('Cart Items: ', cartItems);
+        }
+
         res.send({
             message: 'Get cart endpoint',
-            cart: req.cart
+            cart: cartDataToSend
         });
     }catch(error){
         next(error); 
