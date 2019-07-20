c969e8141d894e1127c98311e70cdc6430a90e32
aemcee <acapili07@gmail.com>
2019-07-20 22:12:34 +0000

    Completed transfer cart to user middleware

---
+++ middleware/cart_to_user.js
@@ -0,0 +1,45 @@
+const db = require(__root + '/db'); 
+
+module.exports = async (req, res, next) => {
+    try{
+        // console.log('Cart to user middleware'); 
+        // console.log('Cart: ', req.cart); 
+
+        // check if cart
+        // if cart, verify cart is active by checking status
+        // add userId to cart
+        // else if no cart, go to next "thing"
+
+        // check if cart userId is equal to req.user id
+        if(req.cart.userId){
+            if(req.cart.userId !== req.user.id){
+                throw new StatusError(401, 'Unauthorized, illegal cart token'); 
+            };
+
+            return next(); 
+        };
+
+        if(req.cart){
+            const [[cartStatus]] = await db.query(`SELECT id FROM cartStatuses WHERE mid="active"`);
+            // console.log('Cart Status: ', cartStatus); 
+
+            if(cartStatus.id === req.cart.cartStatusId){
+                // console.log('The cart is active'); 
+                // console.log('User ID: ', req.user.id);
+                // console.log('Cart ID: ', req.cart.cartId); 
+
+                // now we want to update the cart with the user id
+                // deleted everything before the await
+                await db.query(
+                    `UPDATE carts SET userId=${req.user.id} WHERE id=${req.cart.cartId}`
+                );
+
+                // console.log('Update result: ', updateResult); 
+            };
+        };
+
+        next();
+    }catch(err){
+        next(err);
+    }
+}
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const controllers = require('./controllers');
 const createAccount = require(__root + '/middleware/create_account');
 const signIn = require(__root + '/middleware/sign_in');
+const withCart = require(__root + '/middleware/with_cart');
+const cartToUser = require(__root + '/middleware/cart_to_user'); 
 
 // console.log('routes/auth index.js, router: ', router);
 
@@ -14,9 +16,9 @@ const signIn = require(__root + '/middleware/sign_in');
 // });
 
 // /auth/create-account
-router.post('/create-account', createAccount, controllers.createAccount);
+router.post('/create-account', createAccount, withCart, cartToUser, controllers.createAccount);
 
 // /auth/sign-in
-router.post('/sign-in', signIn, controllers.signIn); 
+router.post('/sign-in', signIn, withCart, cartToUser, controllers.signIn); 
 
 module.exports = router; 
