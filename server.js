69470fca2e6c10fdd9d3f20922972d66ddefe775
aemcee <acapili07@gmail.com>
2019-07-27 22:40:09 +0000

    Server setup for client, client bundled for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { StatusError } = require('./helpers/error_handling');
 const cors = require('cors'); 
+const {resolve} = require('path'); 
 const PORT = process.env.PORT || 9000;
 
 // global access in every file
@@ -14,6 +15,10 @@ const app = express();
 app.use(cors()); 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// act like a static server. build file path and send to static
+// now our website is running off of a single server
+app.use(express.static(resolve(__dirname, 'client', 'dist'))); 
 
 require('./routes')(app);
     
